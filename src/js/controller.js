0d9ffce42cf7f65be164bc619f7282fb692d6e44
Kaarlo Sasiang <sasiang64@gmail.com>
2023-07-30 05:24:36 +0800

    /kaarlo -> updates

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -15,6 +16,8 @@ const controlRecipes = async function () {
 
     if (!id) return;
     recipeView.renderSpinner();
+    // 0) Updates results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -38,15 +41,37 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render Results
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination btns
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // 1) Render New Results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render New pagination btns
+  // paginationView.render(model.state.search);
+  paginationView.update(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addRenderHandler(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addSearchHandler(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
